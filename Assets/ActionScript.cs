d0a785e29bc91114a9845e4e813d19bf754d1d73
SailentzZz <mishatravin99@gmail.com>
2020-05-08 23:49:16 +0300

    first commit

---
+++ Assets/ActionScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void StartScene()
+	{
+		Application.LoadLevel("Scene2");
+	}
+
+	public void Options(GameObject window)
+	{
+		window.SetActive(true);
+	}
+
+	public void Quit()
+	{
+		Application.Quit();
+	}
+}
